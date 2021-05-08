12630271ae22e04e83d7d501c956cff1218aeb70
Sean Sundberg <seansund@us.ibm.com>
2021-05-08 07:56:38 -0500

    Updates test values
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-kms-key.tf
+++ test/stages/stage2-kms-key.tf
@@ -5,6 +5,6 @@ module "kms_key" {
   ibmcloud_api_key = var.ibmcloud_api_key
   name_prefix      = var.name_prefix
   label            = "test-key"
-  provision        = var.kms_key_provision
+  provision        = true
   kms_id           = module.hpcs.guid
 }

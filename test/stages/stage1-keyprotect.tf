af37fc7aecfbae008685252f3bea1f30f72f69b5
balasgit <bala@us.ibm.com>
2022-09-07 12:54:04 -0500

    fix compile errors

--- test/stages/stage1-keyprotect.tf
+++ test/stages/stage1-keyprotect.tf
@@ -1,7 +1,7 @@
 module "keyprotect" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-key-protect.git"
 
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group_name
   region                   = var.region
   name_prefix              = var.name_prefix
   name                     = var.key_protect_name

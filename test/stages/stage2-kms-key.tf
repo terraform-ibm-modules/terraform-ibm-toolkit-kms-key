55b3c078224dd0730dbb798324398e3505315f20
balasgit <bala@us.ibm.com>
2022-09-07 12:52:02 -0500

    fix compile error - RG

--- test/stages/stage2-kms-key.tf
+++ test/stages/stage2-kms-key.tf
@@ -7,4 +7,6 @@ module "kms_key" {
   kms_id           = module.keyprotect.guid
   kms_public_url   = module.keyprotect.public_url
   kms_private_url  = module.keyprotect.private_url
+  resource_group_name = module.resource_group.name
+
 }

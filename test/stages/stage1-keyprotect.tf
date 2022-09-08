bb3ae73da3cc3c8cc1cd8fa582c42b1335d9c3d3
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-08 18:40:38 -0500

    move from CN Repo (#18)
    * move from CN Repo
    
    * added resource group file - fix compile error
    
    * fix compile error - RG
    
    * fix compile errors
    
    * fix compile errors
    
    * Fix compile errors
    
    * Provisioning new KMS by changing flag

--- test/stages/stage1-keyprotect.tf
+++ test/stages/stage1-keyprotect.tf
@@ -1,9 +1,9 @@
 module "keyprotect" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-key-protect.git"
 
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group.name
   region                   = var.region
   name_prefix              = var.name_prefix
   name                     = var.key_protect_name
-  provision                = false
+  provision                = true
 }
